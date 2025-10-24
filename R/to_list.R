eec12d58be35f87b4c8a9baea5f6537e93cc227c
Jon Harmon <jonthegeek@gmail.com>
2025-10-24 08:01:26 -0500

    Implement `to_list()`
    Closes #157.

---
+++ R/to_list.R
@@ -0,0 +1,130 @@
+#' Ensure a list argument meets expectations
+#'
+#' @description `to_list()` checks whether an argument can be coerced to a list
+#'   without losing information, returning it silently if so. Otherwise an
+#'   informative error message is signaled. `to_lst()` is an alias of
+#'   `to_list()`.
+#'
+#' @details This function has three important distinctions from
+#'   [base::as.list()]:
+#' - Functions can be rejects as part of the call to this function (with
+#'   `coerce_function = FALSE`, the default). If they are allowed, they'll be
+#'   coerced to a list concatenating their formals and body (as with
+#'   [base::as.list()].
+#' - Primitive functions (such as [base::is.na()] or [base::is.list()]) always
+#'   throw an error, rather than returning `list(NULL)`.
+#' - `NULL` values can be rejected as part of the call to this function (with
+#'   `allow_null = FALSE`).
+#'
+#' @param ... Arguments passed to [base::as.list()] or other methods.
+#' @inheritParams .shared-params
+#'
+#' @returns The argument as a list.
+#' @export
+to_list <- function(
+  x,
+  ...,
+  x_arg = caller_arg(x),
+  call = caller_env()
+) {
+  UseMethod("to_list")
+}
+
+#' @export
+#' @rdname to_list
+to_lst <- to_list
+
+#' @export
+#' @rdname to_list
+to_list.list <- function(x, ...) {
+  x
+}
+
+#' @export
+#' @rdname to_list
+to_list.default <- function(x, ...) {
+  as.list(x, ...)
+}
+
+#' @export
+#' @rdname to_list
+to_list.NULL <- function(
+  x,
+  ...,
+  allow_null = TRUE,
+  x_arg = caller_arg(x),
+  call = caller_env()
+) {
+  .to_null(x, allow_null = allow_null, x_arg = x_arg, call = call)
+}
+
+#' @export
+#' @rdname to_list
+to_list.function <- function(
+  x,
+  ...,
+  coerce_function = FALSE,
+  x_arg = caller_arg(x),
+  call = caller_env()
+) {
+  .check_function_allowed(x, coerce_function, x_arg = x_arg, call = call)
+  .check_is_not_primitive(x, x_arg = x_arg, call = call)
+  as.list(x, ...)
+}
+
+#' Check whether functions are allowed
+#'
+#' @inheritParams .shared-params-check
+#' @inheritParams .shared-params
+#'
+#' @returns `NULL`, invisibly, if `x` passes the check.
+#' @keywords internal
+.check_function_allowed <- function(
+  x,
+  coerce_function = FALSE,
+  x_arg = caller_arg(x),
+  call = caller_env()
+) {
+  if (is.function(x) && !coerce_function) {
+    .stop_function(x_arg = x_arg, call = call)
+  }
+}
+
+#' Abort because an argument must not be a function
+#'
+#' @inheritParams .stbl_abort
+#' @inheritParams .shared-params
+#'
+#' @returns This function is called for its side effect of throwing an error and
+#'   does not return a value.
+#' @keywords internal
+.stop_function <- function(x_arg, call) {
+  .stop_must(
+    "must not be a {.cls function}.",
+    x_arg = x_arg,
+    call = call,
+    subclass = "bad_function"
+  )
+}
+
+#' Error if an object is a primitive function
+#'
+#' @inheritParams .shared-params-check
+#' @inheritParams .shared-params
+#'
+#' @returns `NULL`, invisibly, if `x` passes the check.
+#' @keywords internal
+.check_is_not_primitive <- function(
+  x,
+  x_arg = caller_arg(x),
+  call = caller_env()
+) {
+  if (is.primitive(x)) {
+    .stop_cant_coerce(
+      "primitive function",
+      "list",
+      x_arg = x_arg,
+      call = call
+    )
+  }
+}
---
+++ tests/testthat/test-to_list.R
@@ -0,0 +1,80 @@
+test_that("to_list() works for lists", {
+  given <- list("a", 1L, TRUE)
+  expect_identical(to_list(given), given)
+})
+
+test_that("to_list() works for NULL", {
+  given <- NULL
+  expect_identical(
+    to_list(given),
+    given
+  )
+})
+
+test_that("to_list() respects allow_null", {
+  given <- NULL
+  expect_error(
+    to_list(given, allow_null = FALSE),
+    class = .compile_dash("stbl", "error", "bad_null")
+  )
+  expect_snapshot(
+    to_list(given, allow_null = FALSE),
+    error = TRUE
+  )
+  expect_snapshot(
+    wrapped_to_list(given, allow_null = FALSE),
+    error = TRUE
+  )
+})
+
+test_that("to_list() works for character vectors", {
+  given <- c("a", "b", "c")
+  expected <- list("a", "b", "c")
+  expect_identical(to_list(given), expected)
+})
+
+test_that("to_list() errors by default for functions", {
+  given <- function(x) x + 1
+  expect_error(
+    to_list(given),
+    class = .compile_dash("stbl", "error", "bad_function")
+  )
+  expect_snapshot(
+    to_list(given),
+    error = TRUE
+  )
+  expect_snapshot(
+    wrapped_to_list(given),
+    error = TRUE
+  )
+})
+
+test_that("to_list() works for functions with coerce_function = TRUE", {
+  given <- function(x) x + 1
+  expected <- as.list(given)
+  expect_identical(
+    to_list(given, coerce_function = TRUE),
+    expected
+  )
+})
+
+test_that("to_list() errors informatively for primitives", {
+  given <- is.na
+  expect_error(
+    to_list(given, coerce_function = TRUE),
+    class = .compile_dash("stbl", "error", "coerce", "list")
+  )
+  expect_snapshot(
+    to_list(given, coerce_function = TRUE),
+    error = TRUE
+  )
+  expect_snapshot(
+    wrapped_to_list(given, coerce_function = TRUE),
+    error = TRUE
+  )
+})
+
+test_that("to_lst() works for lists", {
+  given <- list("a", 1L, TRUE)
+  expect_identical(to_lst(given), to_list(given))
+})
