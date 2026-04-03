6db6f3857700b6c4937dd71c625478bef82ba7d0
Jon Harmon <jonthegeek@gmail.com>
2026-04-03 07:14:42 -0500

    Unify param docs to work with both `x` and `.x`.
    Clean up stabilize_lst.R.

--- R/aaa-shared_params.R
+++ R/aaa-shared_params.R
@@ -63,12 +63,14 @@
 #' @param to_na `(character)` Values to convert to `NA`.
 #' @param to_type_obj An empty object of the target type (e.g., `integer()`).
 #' @param x The argument to stabilize.
-#' @param x_arg `(length-1 character)` An argument name for `x`. The automatic
-#'   value will work in most cases, or pass it through from higher-level
-#'   functions to make error messages clearer in unexported functions.
-#' @param x_class `(length-1 character)` The class name of `x` to use in error
-#'   messages. Use this if you remove a special class from `x` before checking
-#'   its coercion, but want the error message to match the original class.
+#' @param x_arg `(length-1 character)` The name of the argument being stabilized
+#'   to use in error messages. The automatic value will work in most cases, or
+#'   pass it through from higher-level functions to make error messages clearer
+#'   in unexported functions.
+#' @param x_class `(length-1 character)` The class name of the argument being
+#'   stabilized to use in error messages. Use this if you remove a special class
+#'   from the object before checking its coercion, but want the error message to
+#'   match the original class.
 #'
 #' @name .shared-params
 #' @keywords internal
