7a04471872bede81fa4b982f51dbca65a901b49d
Jon Harmon <jonthegeek@gmail.com>
2026-04-06 15:07:21 -0500

    Tag tests
    Co-authored-by: Jon Harmon <jonthegeek@gmail.com>

--- tests/testthat/test-chr_to_lgl.R
+++ tests/testthat/test-chr_to_lgl.R
@@ -1,43 +1,43 @@
-test_that(".chr_to_lgl() converts TRUE/T strings to TRUE", {
+test_that(".chr_to_lgl() converts TRUE/T strings to TRUE (#218)", {
   expect_identical(
     .chr_to_lgl(c("TRUE", "true", "T", "t")),
     c(TRUE, TRUE, TRUE, TRUE)
   )
 })
 
-test_that(".chr_to_lgl() converts FALSE/F strings to FALSE", {
+test_that(".chr_to_lgl() converts FALSE/F strings to FALSE (#218)", {
   expect_identical(
     .chr_to_lgl(c("FALSE", "false", "F", "f")),
     c(FALSE, FALSE, FALSE, FALSE)
   )
 })
 
-test_that(".chr_to_lgl() passes NA through", {
+test_that(".chr_to_lgl() passes NA through (#218)", {
   expect_identical(.chr_to_lgl(NA_character_), NA)
 })
 
-test_that(".chr_to_lgl() converts numeric strings", {
+test_that(".chr_to_lgl() converts numeric strings (#218)", {
   expect_identical(
     .chr_to_lgl(c("0", "1", "-1", "1.5")),
     c(FALSE, TRUE, TRUE, TRUE)
   )
 })
 
-test_that(".chr_to_lgl() returns NA for invalid strings", {
+test_that(".chr_to_lgl() returns NA for invalid strings (#218)", {
   expect_identical(.chr_to_lgl(c("a", "", "NaN")), c(NA, NA, NA))
 })
 
-test_that(".chr_are_lglish() returns TRUE for convertible strings", {
+test_that(".chr_are_lglish() returns TRUE for convertible strings (#218)", {
   expect_identical(
     .chr_are_lglish(c("TRUE", "FALSE", "T", "F", "true", "false", "0", "1")),
     rep(TRUE, 8)
   )
 })
 
-test_that(".chr_are_lglish() returns TRUE for NA", {
+test_that(".chr_are_lglish() returns TRUE for NA (#218)", {
   expect_identical(.chr_are_lglish(NA_character_), TRUE)
 })
 
-test_that(".chr_are_lglish() returns FALSE for invalid strings", {
+test_that(".chr_are_lglish() returns FALSE for invalid strings (#218)", {
   expect_identical(.chr_are_lglish(c("a", "", "NaN")), c(FALSE, FALSE, FALSE))
 })
