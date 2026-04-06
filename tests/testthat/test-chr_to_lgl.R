e37085574bbb830cd41f7fd87dd057e76651b8f6
Jon Harmon <jonthegeek@gmail.com>
2026-04-06 15:01:28 -0500

    Apply air formatting
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/testthat/test-chr_to_lgl.R
+++ tests/testthat/test-chr_to_lgl.R
@@ -1,9 +1,15 @@
 test_that(".chr_to_lgl() converts TRUE/T strings to TRUE", {
-  expect_identical(.chr_to_lgl(c("TRUE", "true", "T", "t")), c(TRUE, TRUE, TRUE, TRUE))
+  expect_identical(
+    .chr_to_lgl(c("TRUE", "true", "T", "t")),
+    c(TRUE, TRUE, TRUE, TRUE)
+  )
 })
 
 test_that(".chr_to_lgl() converts FALSE/F strings to FALSE", {
-  expect_identical(.chr_to_lgl(c("FALSE", "false", "F", "f")), c(FALSE, FALSE, FALSE, FALSE))
+  expect_identical(
+    .chr_to_lgl(c("FALSE", "false", "F", "f")),
+    c(FALSE, FALSE, FALSE, FALSE)
+  )
 })
 
 test_that(".chr_to_lgl() passes NA through", {
@@ -11,7 +17,10 @@ test_that(".chr_to_lgl() passes NA through", {
 })
 
 test_that(".chr_to_lgl() converts numeric strings", {
-  expect_identical(.chr_to_lgl(c("0", "1", "-1", "1.5")), c(FALSE, TRUE, TRUE, TRUE))
+  expect_identical(
+    .chr_to_lgl(c("0", "1", "-1", "1.5")),
+    c(FALSE, TRUE, TRUE, TRUE)
+  )
 })
 
 test_that(".chr_to_lgl() returns NA for invalid strings", {
