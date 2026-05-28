718d1859caa48a76cb1e227032f2dce323cb523e
Jon Harmon <jonthegeek@gmail.com>
2026-05-28 09:25:29 -0500

    air

--- tests/testthat/test-callable_c_functions.R
+++ tests/testthat/test-callable_c_functions.R
@@ -1,30 +1,47 @@
 expected_symbols <- c(
   # chr -> *
-  "stbl_chr_to_lgl", "stbl_chr_are_lglish",
-  "stbl_chr_to_int", "stbl_chr_are_intish",
-  "stbl_chr_to_dbl", "stbl_chr_are_dblish",
+  "stbl_chr_to_lgl",
+  "stbl_chr_are_lglish",
+  "stbl_chr_to_int",
+  "stbl_chr_are_intish",
+  "stbl_chr_to_dbl",
+  "stbl_chr_are_dblish",
   "stbl_chr_are_fctish",
   # dbl -> *
-  "stbl_dbl_to_int", "stbl_dbl_are_intish",
-  "stbl_dbl_to_lgl", "stbl_dbl_are_lglish",
+  "stbl_dbl_to_int",
+  "stbl_dbl_are_intish",
+  "stbl_dbl_to_lgl",
+  "stbl_dbl_are_lglish",
   # int -> *
-  "stbl_int_to_dbl", "stbl_int_are_dblish",
+  "stbl_int_to_dbl",
+  "stbl_int_are_dblish",
   # lgl -> *
-  "stbl_lgl_to_dbl", "stbl_lgl_are_dblish",
-  "stbl_lgl_to_int", "stbl_lgl_are_intish",
+  "stbl_lgl_to_dbl",
+  "stbl_lgl_are_dblish",
+  "stbl_lgl_to_int",
+  "stbl_lgl_are_intish",
   # cpx -> *
-  "stbl_cpx_to_dbl", "stbl_cpx_are_dblish",
-  "stbl_cpx_to_int", "stbl_cpx_are_intish",
+  "stbl_cpx_to_dbl",
+  "stbl_cpx_are_dblish",
+  "stbl_cpx_to_int",
+  "stbl_cpx_are_intish",
   # fct -> *
-  "stbl_fct_to_dbl", "stbl_fct_are_dblish",
-  "stbl_fct_to_int", "stbl_fct_are_intish",
-  "stbl_fct_to_lgl", "stbl_fct_are_lglish",
+  "stbl_fct_to_dbl",
+  "stbl_fct_are_dblish",
+  "stbl_fct_to_int",
+  "stbl_fct_are_intish",
+  "stbl_fct_to_lgl",
+  "stbl_fct_are_lglish",
   "stbl_fct_are_fctish",
   # lst -> *
-  "stbl_lst_to_dbl", "stbl_lst_to_int", "stbl_lst_to_lgl",
-  "stbl_lst_to_chr", "stbl_lst_to_fct",
+  "stbl_lst_to_dbl",
+  "stbl_lst_to_int",
+  "stbl_lst_to_lgl",
+  "stbl_lst_to_chr",
+  "stbl_lst_to_fct",
   # range checks
-  "stbl_check_min_dbl", "stbl_check_max_dbl"
+  "stbl_check_min_dbl",
+  "stbl_check_max_dbl"
 )
 
 # inst/include files -------------------------------------------------------- ----
