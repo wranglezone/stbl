8774118ebe2ca5a0fcba098b096ec94f72c28f86
Jon Harmon <jonthegeek@gmail.com>
2026-09-07 14:35:34 -0500

    Add specify_any_of(), specify_one_of(), and specify_all_of()
    Closes #288

---
+++ tests/testthat/test-specify_all_of.R
@@ -0,0 +1,27 @@
+# specify_all_of ----
+
+test_that("specify_all_of() creates a working specifier (#288)", {
+  spec <- specify_all_of(specify_int(), specify_int_scalar())
+  expect_identical(spec(1L), 1L)
+  expect_s3_class(spec, "stbl_specified_fn")
+})
+
+test_that("specify_all_of() applies each spec to the original x independently (#288)", {
+  spec <- specify_all_of(specify_chr(regex = "^a"), specify_chr(regex = "b$"))
+  expect_identical(spec("ab"), "ab")
+})
+
+test_that("specify_all_of() passes through additional specs from ... (#288)", {
+  spec <- specify_all_of(specify_int())
+  expect_identical(spec(1L, specify_int_scalar()), 1L)
+})
+
+test_that("specify_all_of() errors when a spec fails (#288)", {
+  spec <- specify_all_of(specify_int(), specify_chr())
+  expect_pkg_error_classes(spec("a"), "stbl", "cant_stabilize_all_of")
+})
+
+test_that("specify_all_of() errors when specs disagree on the coerced value (#288)", {
+  spec <- specify_all_of(specify_int(), specify_dbl())
+  expect_pkg_error_classes(spec(1L), "stbl", "inconsistent_all_of")
+})
---
+++ tests/testthat/test-specify_any_of.R
@@ -0,0 +1,28 @@
+# specify_any_of ----
+
+test_that("specify_any_of() creates a working specifier (#288)", {
+  spec <- specify_any_of(specify_int(), specify_chr())
+  expect_identical(spec(1L), 1L)
+  expect_s3_class(spec, "stbl_specified_fn")
+})
+
+test_that("specify_any_of() coerces via the first matching spec (#288)", {
+  spec <- specify_any_of(specify_int(), specify_chr())
+  expect_identical(spec("1"), 1L)
+})
+
+test_that("specify_any_of() passes through additional specs from ... (#288)", {
+  spec <- specify_any_of(specify_int())
+  expect_identical(spec("a", specify_chr()), "a")
+})
+
+test_that("specify_any_of() errors when all specs fail (#288)", {
+  spec <- specify_any_of(specify_int(allow_null = FALSE), specify_chr(allow_null = FALSE))
+  expect_pkg_error_classes(spec(NULL), "stbl", "cant_stabilize_any_of")
+})
+
+test_that("specify_any_of() works nested inside stabilize_lst() (#288)", {
+  int_or_chr <- specify_any_of(specify_int(), specify_chr())
+  given <- list(a = 1L)
+  expect_identical(stabilize_lst(given, a = int_or_chr), given)
+})
